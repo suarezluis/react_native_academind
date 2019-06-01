7afafecce2a8133bd3ab3b63901673e156855d3c
Luis Suarez <suarezluis@gmail.com>
2019-05-31 23:35:45 -0500

    first app init

---
+++ FirstApp/App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default class App extends Component {
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangeHandler = event => {
+    this.setState({
+      placeName: event.val
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChange={this.placeNameChangeHandler}
+          />
+
+          <Button title="Add" style={styles.placeButton} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
+});
